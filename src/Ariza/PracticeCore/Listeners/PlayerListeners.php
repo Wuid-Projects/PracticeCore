b3548dfaa44321af3fec800ba66a54935f5fda01
DeadSafari <deadsafari@deadsafari.me>
2023-09-07 00:40:55 +0500

    Server selector logic

--- src/Ariza/PracticeCore/Listeners/PlayerListeners.php
+++ src/Ariza/PracticeCore/Listeners/PlayerListeners.php
@@ -9,6 +9,11 @@ use pocketmine\player\GameMode;
 use pocketmine\item\VanillaItems;
 use pocketmine\utils\TextFormat as C;
 use Ariza\PracticeCore\Classes\ArizaPlayer;
+use pocketmine\event\player\PlayerItemUseEvent;
+use pocketmine\item\Compass;
+use pocketmine\player\Player;
+use jojoe77777\FormAPI\SimpleForm;
+use pocketmine\event\player\PlayerInteractEvent;
 
 class PlayerListeners implements Listener {
 
@@ -26,5 +31,31 @@ class PlayerListeners implements Listener {
         $compass->setCustomName(C::GREEN . "Select Gamemode!\n" . C::GRAY . "Right Click for PC\nClick the air for Mobile");
         $player->getInventory()->setItem(0, $compass);
         $player->getInventory()->setHeldItemIndex(0);
+    }
+
+    private function ffaSelector(Player $player) {
+        $form = new SimpleForm(function (Player $player, $data) {
+            switch ($data){
+                case 0:
+                    $sumoWorld = $player->getServer()->getWorldManager()->getWorldByName("sumo");
+                    $player->teleport($sumoWorld->getSafeSpawn());
+            }
+        });
+        $form->setTitle("Select a gamemode");
+        $form->addButton("Sumo");
+    }
+
+    public function onItemUse(PlayerItemUseEvent $event) {
+        $item = $event->getItem();
+        if ($item instanceof Compass) {
+            $this->ffaSelector($event->getPlayer());
+        }
+    }
+
+    public function onInteract(PlayerInteractEvent $event) {
+        $item = $event->getItem();
+        if ($item instanceof Compass) {
+            $this->ffaSelector($event->getPlayer());
+        }
     }
 }
